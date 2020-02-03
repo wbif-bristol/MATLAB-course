9ef90c40f0867f869dbb0d4dc08d3355b4fcebf0
Stephen Cross <stephencross86@gmail.com>
2020-02-03 16:53:40 +0000

    Added bits for object oriented programming

---
+++ Session 3 - Advanced data structures/Code/imagestack.m
@@ -0,0 +1,19 @@
+classdef imagestack
+    properties
+        pixels    % 3D pixel array
+        cal       % Spatial calibration in XY
+    end
+    
+    methods
+        % The constructor method
+        function obj = imagestack(pixels)
+            obj.pixels = pixels;
+        end
+        
+        function showImage(obj) 
+           figure(); % Open a new figure
+           obj(:,:,3) = 0; % Convert to RGB
+           imshow(obj); % Display image           
+        end
+    end
+end
